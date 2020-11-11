bbefaa2f57db55bf88e190b4310883a919b3bb40
woof <wooftown@yandex.ru>
2020-11-11 13:01:02 +0300

    8 lesson tests

--- test/lesson8/HeuristicsTestsKotlin.kt
+++ test/lesson8/HeuristicsTestsKotlin.kt
@@ -14,12 +14,6 @@ class HeuristicsTestsKotlin : AbstractHeuristicsTests() {
     @Test
     @Tag("12")
     fun testFindVoyagingPathHeuristics() {
-        findVoyagingPathHeuristics { findVoyagingPathHeuristics(10, 20) }
         findVoyagingPathHeuristics { findVoyagingPathHeuristics(100, 20) }
-        findVoyagingPathHeuristics { findVoyagingPathHeuristics(1000, 20) }
-
-        findVoyagingPathHeuristics { findVoyagingPathHeuristics(10, 10) }
-        findVoyagingPathHeuristics { findVoyagingPathHeuristics(100, 10) }
-        findVoyagingPathHeuristics { findVoyagingPathHeuristics(1000, 10) }
     }
 }
